0347eafd58c9f94a932a79ed0cbf09f3cc33e87c
ThomasGiulianelli <thomas@giulianelli.com>
2019-01-08 23:13:54 -0500

    now calculates a result

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// Thomas Giulianelli
+/* Thomas Giulianelli */
 
 const fetch = require('node-fetch');
+
+/* Variables for storing the response data */
+let myID;
+let operationRequested;
+let leftOperand;
+let rightOperand;
 
 /* GET ID and calculation data to be calculated */
 fetch('https://interview.adpeai.com/api/v1/get-task')
@@ -8,5 +14,34 @@ fetch('https://interview.adpeai.com/api/v1/get-task')
     return response.json();
   })
   .then(function(jsonData) {
-    console.log("Data: " + JSON.stringify(jsonData));
+    myID = jsonData.id;
+    operationRequested = jsonData.operation;
+    leftOperand = jsonData.left;
+    rightOperand = jsonData.right;
+    return calculateResult(operationRequested, leftOperand, rightOperand);
   })
+  .then(function (result) {
+    console.log(result);
+  })
+
+function calculateResult(operation, left, right) {
+  let result;
+  
+  switch(operation) {
+    case "addition" : 
+      result = left + right;
+      break;
+    case "subtraction" :
+      result = left - right;
+      break;
+    case "multiplication" :
+      result = left * right;
+      break;
+    case "division" :
+      result = left / right;
+      break;
+    case "remainder" : 
+      result = left % right;
+  }
+  return result;
+}
