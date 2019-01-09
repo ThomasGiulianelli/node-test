6e37d713d9c1f4d6152fdf7b1c44c4cc1990b886
ThomasGiulianelli <thomas@giulianelli.com>
2019-01-09 17:04:01 -0500

    Now repeatedly processes tasks from the api

--- app.js
+++ app.js
@@ -8,23 +8,38 @@ let operationRequested;
 let leftOperand;
 let rightOperand;
 
-/* GET ID and calculation data to be calculated */
-fetch('https://interview.adpeai.com/api/v1/get-task')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(jsonData) {
-    myID = jsonData.id;
-    operationRequested = jsonData.operation;
-    leftOperand = jsonData.left;
-    rightOperand = jsonData.right;
-    return calculateResult(operationRequested, leftOperand, rightOperand);
-  })
-  .then(function (myResult) {
-    console.log("Result of " + operationRequested + ": " + myResult);
-    let postBody = {id: myID, result: myResult};
-    postResult('https://interview.adpeai.com/api/v1/submit-task', JSON.stringify(postBody));
-  })
+console.log("Getting and submitting tasks continuously...\n");
+main();
+
+function main() {
+  /* GET ID and calculation data to be calculated */
+  fetch('https://interview.adpeai.com/api/v1/get-task')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(jsonData) {
+      myID = jsonData.id;
+      operationRequested = jsonData.operation;
+      leftOperand = jsonData.left;
+      rightOperand = jsonData.right;
+      return calculateResult(operationRequested, leftOperand, rightOperand);
+    })
+    .then(function (myResult) {
+      console.log("Result of " + operationRequested + ": " + myResult);
+      let postBody = {id: myID, result: myResult};
+      return postResult('https://interview.adpeai.com/api/v1/submit-task', JSON.stringify(postBody));
+    })
+    .then(function(completedSuccessfully) {
+      /* Repeat the GET and POST requests after waiting a second */
+      if(completedSuccessfully) {
+        setTimeout(function(){main()}, 1000);
+      }
+      else {
+        console.log("Previous operation did not complete successfully, trying again...\n");
+        setTimeout(function(){main()}, 1000);
+      }
+    })
+}
 
 /* POST result of the calculation */
 function postResult(url = '', data = {}) {
@@ -36,16 +51,18 @@ function postResult(url = '', data = {}) {
     }
   }).then(function(response) {
     if (response.status == 200) {
-      console.log("Success: " + JSON.stringify(response));
+      console.log("Success! Server response: " + JSON.stringify(response) + "\n");
+      return 1;
     }
     else if (response.status == 400) {
-      throw new Error("Incorrect value in result / No ID specified / Value is invalid");
+      throw new Error("Incorrect value in result / No ID specified / Value is invalid\n");
     }
     else if (response.status == 500) {
-      throw new Error("ID cannot be found");
+      throw new Error("ID cannot be found\n");
     }
   }).catch(function(error) {
     console.log("Error: " + error.message);
+    return 0;
   })
 }
 
